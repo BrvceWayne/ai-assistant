4c9802caf2e70019114175644be015e3beba5db3
Normann CHALLAL <normanchallalpro@gmail.com>
2025-11-26 15:46:57 +0100

    Fix asset pipeline issue on Heroku

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,16 +1,22 @@
 module CharactersHelper
   def character_image(character)
-  # Normalisation : minuscules, accents remplacés, espaces -> tirets
-  filename = character.name.downcase
-                         .tr("éèêëàâîïôûùç", "eeeeaaaiiouuc") # accents
-                         .gsub(/[^a-z0-9]+/, "-")              # caractères non-alphanum -> tiret
-                         .gsub(/^-|-$/, "")                     # supprime tirets de début/fin
-                         + ".png"
+    # Normalisation : minuscules, accents remplacés, espaces -> tirets
+    filename = character.name.downcase
+                           .tr("éèêëàâîïôûùç", "eeeeaaaiiouuc") # accents
+                           .gsub(/[^a-z0-9]+/, "-")              # caractères non-alphanum -> tiret
+                           .gsub(/^-|-$/, "")                     # supprime tirets de début/fin
+                           + ".png"
 
-  if Rails.application.assets.find_asset(filename)
-    filename
-  else
-    "placeholder.png"
+    # En production, on fait confiance aux assets précompilés
+    if Rails.env.production?
+      filename
+    else
+      # En développement, on vérifie que le fichier existe
+      if Rails.application.assets && Rails.application.assets.find_asset(filename)
+        filename
+      else
+        "placeholder.png"
+      end
+    end
   end
-end
 end
